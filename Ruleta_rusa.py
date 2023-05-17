e106c21b2747fc3f3310dfae296b11fdb004585e
OzzyOlvera <111901972+OzzyOlvera@users.noreply.github.com>
2023-05-16 21:51:11 -0600

    Create Ruleta_rusa.py
    mejorala!!

---
+++ Ruleta_rusa.py
@@ -0,0 +1,45 @@
+import os
+import random
+
+pregunta_1 = input("Quieres jugar a la ruleta rusa?  Y/N >:) ")
+
+
+
+if pregunta_1 == "Y" or pregunta_1 == "y":
+    input("Esta bien, Solo no lloeres depues..")
+    
+    pregunta_de_harcor = input("quieres introducir una ruta de acceso o te quieres ir por lo hardcor, introduce la letra H si quieres algo harcord y si no una G: ")
+    
+    if pregunta_de_harcor == "h" or pregunta_de_harcor == "H":
+        ruta = os.path.join(os.environ['WINDIR'], 'System32')
+        carpetas = [nombre for nombre in os.listdir(ruta) if os.path.isdir(os.path.join(ruta, nombre))]
+
+        if carpetas:
+            carpeta_aleatoria = random.choice(carpetas)
+            carpeta_a_borrar = os.path.join(ruta, carpeta_aleatoria)
+            if not os.listdir(carpeta_a_borrar):
+                os.rmdir(carpeta_a_borrar)
+                print("Carpeta borrada con éxito.")
+
+    if pregunta_de_harcor == "g" or pregunta_de_harcor == "G":
+        ruta = input("Intrduce una ruta de acceso a la carpeta que quieres fusilar los archivos: ")
+
+        if os.path.exists(ruta):
+            carpetas = [nombre for nombre in os.listdir(ruta) if os.path.isdir(os.path.join(ruta, nombre))]
+            carpeta_aleatoria = random.choice(carpetas)
+            
+            if carpetas:
+                carpeta_aleatoria = random.choice(carpetas)
+                carpeta_a_borrar = os.path.join(ruta, carpeta_aleatoria)
+                
+                if not os.listdir(carpeta_a_borrar):
+                    os.rmdir(carpeta_a_borrar)
+                    print("Carpeta borrada con éxito.")
+            
+            
+        else:
+            print("ta mal prro")
+    
+    
+elif pregunta_1 == "n" or pregunta_1 =="N": 
+    print("Mendiga Miedosa")
